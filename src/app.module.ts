c482c6b45b1c2d11d8bbdc1986052b93206d86ce
hmin01 <goals1003@gmail.com>
2023-05-28 22:41:05 +0900

    2023.05.28 - 환경 변수(Environment various) 설정 - 데이터베이스 연결을 위한 TypeORM 설치 및 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 // Modules
 import { AuthModule } from './apis/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './apis/user/user.module';
+// TypeORM
+import { typeOrmFactory } from './configs/typeorm.config';
 
 @Module({
-  imports: [AuthModule, UserModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmFactory
+    })
+  ]
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const typeOrmFactory = (service: ConfigService): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: '',
-  port: 5432,
-  username: '',
-  password: '',
-  database: '',
+  host: service.get<string | undefined>('DB_HOST'),
+  port: service.get<number | undefined>('DB_PORT'),
+  username: service.get<string | undefined>('DB_USERNAME'),
+  password: service.get<string | undefined>('DB_PASSWORD'),
+  database: service.get<string | undefined>('DB_DATABASE'),
   entities: [],
   synchronize: true
-}
+})
